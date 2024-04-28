da948f21e6a50dcda06abb5e10dbe04c7a89e3db
Moise Guran <moise.guran13@gmail.com>
2024-04-28 16:36:04 +0100

    Vectorised to 9 seconds

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 EXE=d2q9-bgk
 
-CC=icc
+CC=mpiicc
 CFLAGS= -std=c99 -Ofast -ipo -xHost -fopenmp -pg
 LIBS = -lm
 
--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
 
-module load impi/5.1.3.181-iccifortcuda-2016.10
+module load languages/intel/2018-u3
 module load scikit-learn
 # module load languages/gcc/10.4.0 
+# gprof ./d2q9-bgk gmon.out > file
